478d75f65be353790143fea6b1a58efe4fe9c6d9
IsEvrythngTkn <paulngoertzen@gmail.com>
2018-09-26 10:22:49 -0700

    token purchase functionality

--- client.go
+++ client.go
@@ -2,7 +2,16 @@ package main
 
 import (
     "log"
+    "context"
+    "fmt"
+    "strings"
 
+    "encoding/hex"
+    "math/big"
+    "crypto/ecdsa"
+
+    "github.com/ethereum/go-ethereum/core/types"
+    "github.com/ethereum/go-ethereum/crypto"
     "github.com/ethereum/go-ethereum/ethclient"
     "github.com/gin-gonic/gin"
 )
@@ -59,6 +68,68 @@ func main() {
         errorResponse(err, c)
         successResponseString("name", name, c)
     })
+
+    r.POST("/purchaseTokens/:crowdsaleAddress/:privateKey", func(c *gin.Context) {
+        address := c.Param("address")
+        privateKeyHex := c.Param("privateKey")
+        if strings.HasPrefix(privateKeyHex, "0x") {
+            a := []rune(privateKeyHex)
+            privateKeyHex = string(a[2:])
+        }
+        decoded, err := hex.DecodeString(privateKeyHex)
+        if err != nil {
+            //log.Fatal("here")
+            log.Fatal(err)
+        }
+
+        privateKey, err := crypto.ToECDSA(decoded)
+        if err != nil {
+            log.Fatal(err)
+        }
+
+        publicKey := privateKey.Public()
+        publicKeyECDSA, ok := publicKey.(*ecdsa.PublicKey)
+        if !ok {
+            log.Fatal("error casting public key to ECDSA")
+        }
+
+        fromAddress := crypto.PubkeyToAddress(*publicKeyECDSA)
+        nonce, err := client.PendingNonceAt(context.Background(), fromAddress)
+        if err != nil {
+            log.Fatal(err)
+        }
+
+        value := big.NewInt(1000000000000000000) // in wei (1 eth)
+        gasLimit := uint64(21000)                // in units
+        gasPrice, err := client.SuggestGasPrice(context.Background())
+        if err != nil {
+            log.Fatal(err)
+        }
+
+        var data []byte
+        toAddress := getAddress(address)
+        tx := types.NewTransaction(nonce, toAddress, value, gasLimit, gasPrice, data)
+
+        chainID, err := client.NetworkID(context.Background())
+        if err != nil {
+            log.Fatal(err)
+        }
+
+        signedTx, err := types.SignTx(tx, types.NewEIP155Signer(chainID), privateKey)
+        if err != nil {
+            log.Fatal(err)
+        }
+
+        err = client.SendTransaction(context.Background(), signedTx)
+        if err != nil {
+            log.Fatal(err)
+        }
+
+        transactionString := signedTx.Hash().Hex()
+        fmt.Printf(transactionString)
+
+        successResponseString("transaction", transactionString, c)
+    });
 
     r.Run() // listen and serve on 0.0.0.0:8080
 }
